f621ebfc66203ad181c6b14c8a0cb842cdef224b
Alexandru Miclea <alexmiclea@hephaestus.local>
2026-03-07 12:42:40 +0200

    Initial commit

---
+++ src/main/java/org/alexmiclea/reptopetrol/ReptopetrolApplication.java
@@ -0,0 +1,13 @@
+package org.alexmiclea.reptopetrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReptopetrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReptopetrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/alexmiclea/reptopetrol/ReptopetrolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alexmiclea.reptopetrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReptopetrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
